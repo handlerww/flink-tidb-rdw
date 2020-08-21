2547fafb0c82834740b10e48c38b581c50a48160
Zhi Qi <1578166061@qq.com>
2020-08-21 19:50:32 +0800

    naive test.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,8 +4,6 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.Table;
-import org.apache.flink.api.java.utils.ParameterTool;
-import static main.Sqls.*;
 
 public class Main {
 
@@ -15,76 +13,58 @@ public class Main {
                 EnvironmentSettings.newInstance().useBlinkPlanner().inStreamingMode().build()
         );
 
-        ParameterTool parameter = ParameterTool.fromArgs(args);
-        String destination_host = parameter.get("dest_host", "127.0.0.1");
-        String source_host = parameter.get("source_host", "127.0.0.1");
-
-        //test(tEnv, source_host, destination_host);
-        tpcc(tEnv, source_host, destination_host);
-    }
-    static void test(StreamTableEnvironment tEnv, String source_host, String destination_host) {
-        tEnv.executeSql(getCreateTableSql("base") +
-                getSourceWith(source_host, "test", "base"));
-        tEnv.executeSql(getCreateTableSql("stuff") +
-                getSourceWith(source_host, "test", "stuff"));
-        tEnv.executeSql(getCreateTableSql("wide_stuff") +
-                getSinkWith(destination_host, "test", "wide_stuff"));
-
+        tEnv.executeSql("create table base (\n" +
+                "\tbase_id int primary key,\n" +
+                "\tbase_location varchar(20)\n" +
+                ") WITH (\n" +
+                "\t'connector' = 'kafka',\n" +
+                "\t'topic' = 'test-base',\n" +
+                "\t'properties.group.id' = 'testGroup',\n" +
+                "\t'scan.startup.mode' = 'latest-offset',\n" +
+                "\t'properties.bootstrap.servers' = '127.0.0.1:9092',\n" +
+                "\t'format' = 'canal-json',\n" +
+                "\t'canal-json.ignore-parse-errors'='true'\n" +
+                ")"
+        );
+        tEnv.executeSql("create table stuff(\n" +
+                "\tstuff_id int primary key,\n" +
+                "\tstuff_base_id int,\n" +
+                "\tstuff_name varchar(20)\n" +
+                ") WITH (\n" +
+                "\t'connector' = 'kafka',\n" +
+                "\t'topic' = 'test-stuff',\n" +
+                "\t'properties.group.id' = 'testGroup',\n" +
+                "\t'scan.startup.mode' = 'latest-offset',\n" +
+                "\t'properties.bootstrap.servers' = '127.0.0.1:9092',\n" +
+                "\t'format' = 'canal-json',\n" +
+                "\t'canal-json.ignore-parse-errors'='true'\n" +
+                ")"
+        );
+        tEnv.executeSql("create table wide_stuff(\n" +
+                "\tstuff_id int primary key,\n" +
+                "\tbase_id int,\n" +
+                "\tbase_location varchar(20),\n" +
+                "\tstuff_name varchar(20)\n" +
+                ") WITH (\n" +
+                "\t'connector'  = 'jdbc',\n" +
+                "\t'url'        = 'jdbc:mysql://127.0.0.1:4000/test?rewriteBatchedStatements=true',\n" +
+                "\t'table-name' = 'wide_stuff',\n" +
+                "\t'driver'     = 'com.mysql.cj.jdbc.Driver',\n" +
+                "\t'username'   = 'root',\n" +
+                "\t'password'   = ''\n" +
+                ")"
+        );
         printSource(tEnv, "base");
         printSource(tEnv, "stuff");
 
         tEnv.executeSql("CREATE TABLE print_wide_stuff WITH ('connector' = 'print') LIKE wide_stuff (EXCLUDING ALL)");
         Table t = tEnv.sqlQuery(
                 "select stuff.stuff_id, base.base_id, base.base_location, stuff.stuff_name\n" +
-                        "from stuff inner join base\n" +
-                        "on stuff.stuff_base_id = base.base_id"
+                    "from stuff inner join base\n" +
+                    "on stuff.stuff_base_id = base.base_id"
         );
         t.executeInsert("wide_stuff");
         t.executeInsert("print_wide_stuff");
-    }
-
-    static void tpcc(StreamTableEnvironment tEnv, String source_host, String destination_host) {
-        String[] tpccSourceTableNames = {"customer", "district", "history", "item", "new_order", "order_line", "orders", "stock", "warehouse"};
-        for(String tableName: tpccSourceTableNames) {
-            tEnv.executeSql(getCreateTableSql(tableName) +
-                    getSourceWith(source_host, "tpcc", tableName));
-            //printSource(tEnv, tableName);
-        }
-
-        String[] tpccSinkTableNames = {"wide_customer_warehouse", "wide_new_order", "wide_order_line_district"};
-        for(String tableName: tpccSinkTableNames) {
-            tEnv.executeSql(getCreateTableSql(tableName) +
-                    getSinkWith(destination_host, "tpcc", tableName));
-        }
-
-        tEnv.sqlQuery(
-                "select ol_o_id, ol_d_id, ol_w_id, ol_number, ol_i_id, ol_supply_w_id," +
-                        " ol_delivery_d, ol_quantity, ol_amount, ol_dist_info, d_name," +
-                        " d_street_1, d_street_2, d_city, d_state, d_zip, d_tax, d_ytd, d_next_o_id " +
-                "from order_line left join district " +
-                "on order_line.ol_d_id = district.d_id " +
-                "and order_line.ol_w_id = district.d_w_id"
-        ).executeInsert("wide_order_line_district");
-
-        tEnv.sqlQuery(
-                "select no_o_id, no_d_id, no_w_id, d_name, d_street_1, d_street_2, d_city, " +
-                "d_state, d_zip, d_tax, d_ytd, d_next_o_id, w_name, w_street_1, " +
-                "w_street_2, w_city, w_state, w_zip, w_tax, w_ytd " +
-                        "from new_order left join district " +
-                        "on new_order.no_d_id = district.d_id " +
-                        "and new_order.no_w_id = district.d_w_id " +
-                "left join warehouse " +
-                "on new_order.no_w_id = warehouse.w_id"
-        ).executeInsert("wide_new_order");
-
-        tEnv.sqlQuery(
-                "select c_id, c_d_id, c_w_id, c_first, c_middle, c_last, c_street_1, c_street_2," +
-                        " c_city, c_state, c_zip, c_phone, c_since, c_credit, c_credit_lim," +
-                        " c_discount, c_balance, c_ytd_payment, c_payment_cnt, c_delivery_cnt," +
-                        " c_data, w_name, w_street_1, w_street_2, w_city, w_state, w_zip, w_tax, w_ytd " +
-                "from customer left join warehouse " +
-                "on customer.c_w_id=warehouse.w_id"
-        ).executeInsert("wide_customer_warehouse");
     }
 
     /**
